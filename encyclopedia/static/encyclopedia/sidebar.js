a9abc2f1dc840a5573c6da40d0a60b40b1616ace
Alfio Martini <alfio.martini@gmail.com>
2020-09-08 17:31:50 -0300

    Added scrollIntoView experience for smaller devices

--- encyclopedia/static/encyclopedia/sidebar.js
+++ encyclopedia/static/encyclopedia/sidebar.js
@@ -8,3 +8,13 @@ export function sidebar_height(max_width, small_height, big_height){
         sidebar.style.height = `${big_height}vh`; 
     }
 }
+
+export function isMaxHeight(max_width){
+    const mediaq = window.matchMedia( `(max-width: ${max_width}px)` );
+    if (mediaq.matches){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
